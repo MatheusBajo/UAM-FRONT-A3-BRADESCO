f19bcaa1796b1749daf40cdec6b4e51548e7e936
Matheus Bajo <matheusbajo@gmail.com>
2025-06-12 19:08:08 -0300

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite'
-import path from "path"
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  server: { proxy: { '/api': 'http://localhost:8080' } },
+  plugins: [react()],
   base: '/UAM-FRONT-A3-BRADESCO/',
-  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      '@': path.resolve(__dirname, './src')
+    }
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 })
